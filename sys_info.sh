6212858fa6cfe2496589197a7c9b58383f9e40ab
cody-pearson <cpearson1204@gmail.com>
2017-03-03 09:06:38 -0600

    Update sys_info.sh

--- sys_info.sh
+++ sys_info.sh
@@ -1,40 +1,52 @@
 #!/bin/bash 
 #==========================================================
-#title           :sys_info.sh
-#description     :RHEL 6 system info script.
-#author          :Pearson
-#date            :2017 Feb 5
-#version         :1.0    
-#usage           :bash sys_info.sh or ./sys_info.sh
-#notes           :Need root or sudo permissions to run.
+# title           : sys_info.sh
+# description     : RHEL 6 system info script.
+# author          : Pearson
+# date            : 2017 Feb 5 - 2017 Mar 3
+# version         : 1.1
+# usage           : bash sys_info.sh or ./sys_info.sh
+# notes           : Need root or sudo permissions to run.
+# TODO            : 1) More precise reporting
+#                 : 2) Add more "meaningful" metrics
 #==========================================================
 
 #Variables
 hn=$(hostname -a)
-inf_log=/path/to/"$hn"_sys_info
+today=$(date +%F)
+info_log=/path/to/"$hn"_sys_info."$today"
 
-## Check to see if root is running
-if [[ "$USER" != "root" ]]; then
+## Run as root
+if [[ "$UID" -ne "0" ]]; then
   echo "You need to be root to run this script!!"
   exit
 fi
 
 ## Check for sysinfo file
 if [[ -f "$info_log" ]]; then
-  echo ""$hn" appears to already have a system info file"
+  echo "$hn appears to already have a system info file"
   read -p "Would you like to delete it? [Y]/[N]  "  dfile
     if [[ "$dfile" == [Yy][Ee][Ss] ]] || [[ "$dfile" == [Yy] ]]; then
-      echo "Deleting "$info_file"
-      rm -f "$info_log"
-      echo "Creating a new info file for "$hn"  "
-    else
-      echo "You may want to copy "$info_log"
+      echo "Deleting $info_log"
+      rm -f $info_log
+      echo "Creating a new info file for $hn  "
+    elif [[ "$dfile" == [Nn][Oo] ]] || [[ "$dfile" == [Nn] ]]; then
+      echo "You may want to copy $info_log"
       echo "to another location if you want to keep this file for"
       echo "archiving purposes and then rerun this script..."
       exit
+    else
+      echo "That was not a valid answer...."
+      exit
     fi
 else
-  touch "$info_log"
+  touch $info_log 2> /dev/null
+    if [[ "$?" -ne "0" ]]; then
+      echo "***ERROR*** There was an issue creating the sys_info file"
+      exit
+    else
+      echo "The sys_info file is located here:  $info_log   "
+    fi
 fi
 
 ## Hostname
@@ -57,9 +69,6 @@ echo "Getting MAC Address"
 echo -n "MAC Addresses:  " >> "$info_log"
 ifconfig -a | grep -i HWaddr >> "$info_log"
 
-echo "Your system info file is located at "$info_log"  "
+echo ""
+echo "Your sys_info file is located at "$info_log"  "
 
-
-## TODO:
-### script isn't as precise as I want... will continue to refine searches  //CP
-### Obviously more metrics can be found... Depends on requested resource  //CP
